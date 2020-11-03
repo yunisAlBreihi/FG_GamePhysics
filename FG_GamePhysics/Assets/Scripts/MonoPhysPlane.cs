70933c496d373998fb3ec5691e47d0af613efbe1
CM\yunis.al.breihi <yunis.albreihi@gmail.com>
2020-11-03 17:23:40 +0100

    added new code.

---
+++ FG_GamePhysics/Assets/Scripts/MonoPhysPlane.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using UnityEngine;
+
+public class MonoPhysPlane : MonoBehaviour
+{
+    Vector3 Normal => transform.up;
+    Vector3 Position => transform.position;
+    public float Distance(PhysicsSphere sphere)
+    {
+        Vector3 sphereToPlane = Position - sphere.transform.position;
+
+        return Vector3.Dot(sphereToPlane, Normal);
+    }
+        
+    public Vector3 Projection(PhysicsSphere sphere)
+    {
+        Vector3 sphereToProjection = Distance(sphere) * Normal;
+
+        return sphere.transform.position + sphereToProjection;
+    }
+
+    public bool isColliding(PhysicsSphere sphere)
+    {
+        if (WillBeCollision(sphere) == false)
+        {
+            return false;
+        }
+        return Distance(sphere)>=0 || Mathf.Abs(Distance(sphere)) <= sphere.Radius;
+    }
+
+    private bool WillBeCollision(PhysicsSphere sphere)
+    {
+        return Vector3.Dot(sphere.Velocity, Normal) < 0;
+    }
+
+    public Vector3 CorrectedPosition(PhysicsSphere sphere)
+    {
+        return Projection(sphere) + Normal * sphere.Radius;
+    }
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="sphere"></param>
+    /// <param name="energyDissipation" the impact of the energy dissapation on the reflected velocity></param>
+    public void Chock(PhysicsSphere sphere, float energyDissipation =0)
+    {
+        if (isColliding(sphere) == false)
+        {
+            return;
+        }
+
+        //Debug.Log("Velocity Error: isVerlet " + sphere.isVerlet + " " + sphere.ErrorVelocityOnGround());
+
+        if (IsSphereStatic(sphere))
+        {
+            sphere.transform.position = CorrectedPosition(sphere);
+            sphere.ApplyForce(-sphere.Mass * Physics.gravity);
+        }
+        else //sphere is dynamic 
+        {
+            sphere.Velocity = Reflect(sphere.Velocity, energyDissipation);
+        }
+    }
+
+    private Vector3 Reflect(Vector3 velocity, float energyDissipation = 0)
+    {
+        return (velocity - 2f * Vector3.Dot(velocity, Normal) * Normal) * (1f-energyDissipation);
+    }
+
+    bool IsSphereStatic(PhysicsSphere sphere)
+    {
+        bool lowVelocity = sphere.Velocity.magnitude < 0.2f;
+        bool touchingThePlane = (CorrectedPosition(sphere) - sphere.transform.position).magnitude < 0.02f;
+        return lowVelocity && touchingThePlane;
+    }
+}
---
+++ FG_GamePhysics/Assets/Scripts/PhysicsSphere.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using UnityEngine;
+
+public class PhysicsSphere : MonoBehaviour
+{
+    [SerializeField]
+    Vector3 velocity = Vector3.zero;
+    public Vector3 Velocity { get => velocity; set => velocity = value; }
+    
+    [SerializeField]
+    float mass = 1f;
+
+    public float Mass { get => mass; set => mass = value; }
+
+    float drag = 0.1f;
+
+    public bool isVerlet = true;
+
+    [SerializeField]
+    bool useGravity = true;
+
+    public MonoPhysPlane plane = null;
+
+    [SerializeField]
+    float chockEnergyDissapation = 0.05f;
+    public float Radius => transform.localScale.x * 0.5f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        ApplyForce(new Vector3(0f,0f,0f));
+
+        Vector3 hitPoint = plane.Projection(this);
+        bool isColliding = plane.isColliding(this);
+
+        Debug.DrawLine(transform.position, plane.Projection(this), Color.red);
+
+        plane.Chock(this, chockEnergyDissapation);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            ApplyForce(new Vector3(10.0f, 0f, 0f));
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            ApplyForce(new Vector3(-10.0f, 0f, 0f));
+        }
+    }
+    void CorrectPosition(bool isColliding, Vector3 hitPoint)
+    {
+        if (isColliding == false)
+            return;
+
+        Vector3 correctedPosition = plane.CorrectedPosition(this);
+        Debug.DrawLine(hitPoint, correctedPosition, Color.green);
+
+        transform.position = correctedPosition;
+    }
+
+    /// <summary>
+    /// Euler integration
+    /// </summary>
+    /// <param name="force"></param>
+    public void ApplyForce(Vector3 force)
+    {
+        Vector3 totalForce = useGravity ? force + mass * Physics.gravity : force;
+
+        //f = m * a
+        //a = f / m 
+        Vector3 acc = totalForce / mass;
+
+        Integrate(acc, isVerlet);
+    }
+
+    void Integrate(Vector3 acc, bool isVerlet = false)
+    {
+        if (isVerlet == false)
+        {
+            //v1 = v0 + a * deltatime
+            velocity = velocity + acc * Time.deltaTime;
+
+            //p1 = p0 + v * deltatime
+            transform.position = transform.position + velocity * Time.deltaTime;
+        }
+        else //use verlete
+        {
+            transform.position += velocity * Time.deltaTime + acc * Time.deltaTime * Time.deltaTime * 0.5f;
+            velocity += acc * Time.deltaTime ;
+
+     
+        }
+    }
+
+    /// <summary>
+    /// Assuming the ground is at y = 0 and the intitial velocity is 0
+    /// </summary>
+    /// <returns></returns>
+    public float VelocityOnGround()
+    {
+        return Mathf.Sqrt(2f * Physics.gravity.magnitude * (transform.position.y - plane.transform.position.y));
+    }
+    public float ErrorVelocityOnGround()
+    {
+        return Mathf.Abs(velocity.magnitude - VelocityOnGround());
+    }
+}
